95f1e0721fc9d751316e676bf53f044a2488bf5c
ingenuity <dimakorets@dimas-MacBook-Pro.local>
2024-02-20 17:10:08 +0100

    same docker config as for exchanger-core

---
+++ config/healthcheck.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'targets' => [
+        1 => 'https://obmenoff.cc',
+        2 => 'https://uachanger.com',
+        4 => 'https://kursoff.biz',
+        5 => 'https://best-obmen.com',
+        6 => 'https://first-btc.com',
+        7 => 'https://wx.money',
+        8 => 'https://2rbina.net',
+        9 => 'https://btc2pay.org',
+        10 => 'https://1wm.io',
+        11 => 'https://vobmenka.com',
+        12 => 'https://kz007.org',
+        14 => 'https://wm.money',
+        15 => 'https://4esnok.cc',
+        16 => 'https://appbit.net',
+        17 => 'https://nextbit.me',
+        20 => 'https://fastex.su',
+    ]
+];
