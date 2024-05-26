eec0173d8d90063b13bb51ec2d6372bdae071b2c
ingenuity <dimakorets@dimas-MacBook-Pro.local>
2024-05-26 21:54:07 +0200

    handle no data db query

--- app/Http/Controllers/MetricsController.php
+++ app/Http/Controllers/MetricsController.php
@@ -27,90 +27,93 @@ class MetricsController extends Controller
         $yesterdayDate = Carbon::now()->subDay()->toDateString();
 
         foreach ($observedSites as $brandId => $observedSite) {
-
             $checkedRegistryItem = HealthcheckRegistryItem::where('brand_id', $brandId)
                 ->where('date', $todayDate)
                 ->first();
 
-            $checksCount = $registry->registerGauge(
-                $appName,
-                MetricName::TODAY_CHECKS_LOADING_TIMES->value . $brandId,
-                'today loading times count',
-                ['site_name']
-            );
+            if (null !== $checkedRegistryItem) {
+                $checksCount = $registry->registerGauge(
+                    $appName,
+                    MetricName::TODAY_CHECKS_LOADING_TIMES->value . $brandId,
+                    'today loading times count',
+                    ['site_name']
+                );
 
-            $checksCount->set($checkedRegistryItem->total_checks, [$checkedRegistryItem->site_name]);
+                $checksCount->set($checkedRegistryItem->total_checks, [$checkedRegistryItem->site_name]);
 
-            $failedChecksCount = $registry->registerGauge(
-                $appName,
-                MetricName::TODAY_FAILED_CHECKS_LOADING_TIMES->value . $brandId,
-                'today failed loading times count',
-                ['site_name']
-            );
+                $failedChecksCount = $registry->registerGauge(
+                    $appName,
+                    MetricName::TODAY_FAILED_CHECKS_LOADING_TIMES->value . $brandId,
+                    'today failed loading times count',
+                    ['site_name']
+                );
 
-            $failedChecksCount->set($checkedRegistryItem->failed_checks, [$checkedRegistryItem->site_name]);
+                $failedChecksCount->set($checkedRegistryItem->failed_checks, [$checkedRegistryItem->site_name]);
 
 
-            $averageLoadingTime = $registry->registerGauge(
-                $appName,
-                MetricName::TODAY_AVERAGE_LOADING_TIME->value . $brandId,
-                'today avg loading time',
-                ['site_name']
-            );
+                $averageLoadingTime = $registry->registerGauge(
+                    $appName,
+                    MetricName::TODAY_AVERAGE_LOADING_TIME->value . $brandId,
+                    'today avg loading time',
+                    ['site_name']
+                );
 
-            $averageLoadingTime->set($loadingTimeMetrics->getAverageLoadingTime($checkedRegistryItem), [$checkedRegistryItem->site_name]);
+                $averageLoadingTime->set($loadingTimeMetrics->getAverageLoadingTime($checkedRegistryItem), [$checkedRegistryItem->site_name]);
 
-            $todayLoadingTimes = $registry->registerCounter(
-                $appName,
-                MetricName::TODAY_LOADING_TIMES->value . $brandId,
-                'Today loading times',
-                ['time', 'site_name']
-            );
+                $todayLoadingTimes = $registry->registerCounter(
+                    $appName,
+                    MetricName::TODAY_LOADING_TIMES->value . $brandId,
+                    'Today loading times',
+                    ['time', 'site_name']
+                );
 
-            foreach ($loadingTimeMetrics->getLoadingTimes($checkedRegistryItem) as $row) {
-                $todayLoadingTimes->incBy($row['loading_time'], [$row['time'], $row['site_name']]);
+                foreach ($loadingTimeMetrics->getLoadingTimes($checkedRegistryItem) as $row) {
+                    $todayLoadingTimes->incBy($row['loading_time'], [$row['time'], $row['site_name']]);
+                }
             }
 
             $yesterdayCheckedRegistryItem = HealthcheckRegistryItem::where('brand_id', $brandId)
                 ->where('date', $yesterdayDate)
                 ->first();
 
-            $yesterdayChecksCount = $registry->registerGauge(
-                $appName,
-                MetricName::YESTERDAY_CHECKS_LOADING_TIMES->value . $brandId,
-                'yesterday loading times count',
-                ['site_name']
-            );
+            if (null !== $yesterdayCheckedRegistryItem) {
+                $yesterdayChecksCount = $registry->registerGauge(
+                    $appName,
+                    MetricName::YESTERDAY_CHECKS_LOADING_TIMES->value . $brandId,
+                    'yesterday loading times count',
+                    ['site_name']
+                );
 
-            $yesterdayChecksCount->set($yesterdayCheckedRegistryItem->total_checks, [$yesterdayCheckedRegistryItem->site_name]);
+                $yesterdayChecksCount->set($yesterdayCheckedRegistryItem->total_checks, [$yesterdayCheckedRegistryItem->site_name]);
 
-            $yesterdayFailedChecksCount = $registry->registerGauge(
-                $appName,
-                MetricName::YESTERDAY_FAILED_CHECKS_LOADING_TIMES->value . $brandId,
-                'yesterday failed loading times count',
-                ['site_name']
-            );
+                $yesterdayFailedChecksCount = $registry->registerGauge(
+                    $appName,
+                    MetricName::YESTERDAY_FAILED_CHECKS_LOADING_TIMES->value . $brandId,
+                    'yesterday failed loading times count',
+                    ['site_name']
+                );
 
-            $yesterdayFailedChecksCount->set($yesterdayCheckedRegistryItem->failed_checks, [$yesterdayCheckedRegistryItem->site_name]);
+                $yesterdayFailedChecksCount->set($yesterdayCheckedRegistryItem->failed_checks, [$yesterdayCheckedRegistryItem->site_name]);
 
-            $yesterdayAverageLoadingTime = $registry->registerGauge(
-                $appName,
-                MetricName::YESTERDAY_AVERAGE_LOADING_TIME->value . $brandId,
-                'yesterday avg loading time',
-                ['site_name']
-            );
+                $yesterdayAverageLoadingTime = $registry->registerGauge(
+                    $appName,
+                    MetricName::YESTERDAY_AVERAGE_LOADING_TIME->value . $brandId,
+                    'yesterday avg loading time',
+                    ['site_name']
+                );
 
-            $yesterdayAverageLoadingTime->set($loadingTimeMetrics->getAverageLoadingTime($yesterdayCheckedRegistryItem), [$yesterdayCheckedRegistryItem->site_name]);
+                $yesterdayAverageLoadingTime->set($loadingTimeMetrics->getAverageLoadingTime($yesterdayCheckedRegistryItem), [$yesterdayCheckedRegistryItem->site_name]);
 
-            $yesterdayLoadingTimes = $registry->registerCounter(
-                $appName,
-                MetricName::YESTERDAY_LOADING_TIMES->value . $brandId,
-                'Yesterday loading times',
-                ['time', 'site_name']
-            );
+                $yesterdayLoadingTimes = $registry->registerCounter(
+                    $appName,
+                    MetricName::YESTERDAY_LOADING_TIMES->value . $brandId,
+                    'Yesterday loading times',
+                    ['time', 'site_name']
+                );
 
-            foreach ($loadingTimeMetrics->getLoadingTimes($yesterdayCheckedRegistryItem) as $row) {
-                $yesterdayLoadingTimes->incBy($row['loading_time'], [$row['time'], $row['site_name']]);
+                foreach ($loadingTimeMetrics->getLoadingTimes($yesterdayCheckedRegistryItem) as $row) {
+                    $yesterdayLoadingTimes->incBy($row['loading_time'], [$row['time'], $row['site_name']]);
+                }
             }
         }
 
