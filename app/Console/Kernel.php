2fd94bad4448b435ff0a9e42016fb484bed249da
ingenuity <dimakorets@dimas-MBP.home>
2024-10-21 18:34:35 +0300

    add website speed metrics

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use App\Console\Commands\BestchangeObserverCommand;
 use App\Console\Commands\HealthcheckCommand;
+use App\Console\Commands\PingCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command(HealthcheckCommand::class)->everyMinute()->withoutOverlapping(10);
-        $schedule->command(HealthcheckCommand::class)->everyMinute()->withoutOverlapping(10);
+        $schedule->command(PingCommand::class)->everyMinute()->withoutOverlapping(10);
         $schedule->command(BestchangeObserverCommand::class)->everyMinute()->withoutOverlapping(10);
     }
 
